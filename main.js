46255d2caf74e9ff636343df45a452e181e7cc8b
unknown <traudyjose@gmail.com>
2023-11-20 14:19:58 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,29 @@
+//main.js
+<footer>
+    
+    <
+<a href="https://web.WhatsApp.com" target="_blank"><img src="logowhat.jpg" alt="Logo WhatsApp" width="30px" class="whatsapp-icon"></a>
+    
+   
+<a href="https://www.instagram.com" target="_blank"><img src="logoig.jpg" alt="Logo Instagram" width="30px"></a>
+    
+   
+<a href="https://www.facebook.com" target="_blank"><img src="logofb.jpg" alt="Logo Facebook" width="30px"></a>
+    <p>&copy; 2023 Hoteles Increíbles. Todos los derechos reservados.</p>
+
+</footer
+
+</fo
+</footer>
+
+< />
+</>!-- Añadir el siguiente script al final del body -->
+<script>
+function abrirChat() {
+    var numeroTelefono = '1167921663';
+    var url = 'https://wa.me/+541167921663' + numeroTelefono;
+    window.open(url, '_blank');
+}
+    
+    < /></>!-- ... otros scripts y enlaces ... -->
+
